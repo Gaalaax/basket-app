027625befcb3da93969e20a89f3d1d0c0a295e3b
Yagiz Yasar <yagizyasar2000@hotmail.com>
2024-03-26 19:01:54 +0300

    urun fotograflari eklendi ve tanimlandi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {
+  Container,
+  SimpleGrid,
+  List,
+  ThemeIcon,
+  rem,
+  Input,
+} from "@mantine/core";
+import { IconCircleCheck, IconCircleDashed } from "@tabler/icons-react";
+import Card from "./Components/Card";
+import "./App.css";
+
+const storeItems = [
+  {
+    name: "Camera",
+    src: "camera",
+    price: 20,
+  },
+  {
+    name: "Headphone",
+    src: "headphone",
+    price: 10,
+  },
+  {
+    name: "Camera Lens",
+    src: "lens",
+    price: 25,
+  },
+  {
+    name: "Retro Camera",
+    src: "retro-cam",
+    price: 25,
+  },
+  {
+    name: "Toy Car",
+    src: "toy-car",
+    price: 25,
+  },
+  {
+    name: "Watch",
+    src: "watch",
+    price: 25,
+  },
+];
 
 function App() {
+  let [basketItems, setBasketItems] = useState([]);
+  let [searchValue, setSearchValue] = useState("");
+  let filteredItems = basketItems.filter(
+    (item) => item.name.toLowerCase().indexOf(searchValue.toLowerCase()) >= 0
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <SimpleGrid cols={3} className="Store">
+        {storeItems.map(({ name, src }) => {
+          return (
+            <Card
+              key={name}
+              src={src}
+              name={name}
+              onAdd={() => setBasketItems([...basketItems, { name }])}
+            />
+          );
+        })}
+      </SimpleGrid>
+      <Input.Wrapper label="Search ">
+        <Input onChange={(e) => setSearchValue(e.target.value)} />
+      </Input.Wrapper>
+      <List
+        class="list"
+        spacing="xs"
+        size="sm"
+        center
+        icon={
+          <ThemeIcon color="teal" size={24} radius="xl">
+            <IconCircleCheck style={{ width: rem(16), height: rem(16) }} />
+          </ThemeIcon>
+        }
+      >
+        {filteredItems.map(({ name }, index) => (
+          <List.Item key={index}>{name}</List.Item>
+        ))}
+      </List>
+    </Container>
   );
 }
 
