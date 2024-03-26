2f3f5cfa68fac780878cfe5bac5cd5f5897d9fab
Yagiz Yasar <yagizyasar2000@hotmail.com>
2024-03-26 19:18:46 +0300

    sepet filtreleme yerine urun filtreleme yapildi ama ikisini birden yapiyor

--- src/App.js
+++ src/App.js
@@ -47,13 +47,16 @@ const storeItems = [
 function App() {
   let [basketItems, setBasketItems] = useState([]);
   let [searchValue, setSearchValue] = useState("");
-  let filteredItems = basketItems.filter(
+  let filteredItems = storeItems.filter(
     (item) => item.name.toLowerCase().indexOf(searchValue.toLowerCase()) >= 0
   );
   return (
     <Container>
+      <Input.Wrapper label="Search ">
+        <Input onChange={(e) => setSearchValue(e.target.value)} />
+      </Input.Wrapper>
       <SimpleGrid cols={3} className="Store">
-        {storeItems.map(({ name, src }) => {
+        {filteredItems.map(({ name, src }) => {
           return (
             <Card
               key={name}
@@ -64,9 +67,7 @@ function App() {
           );
         })}
       </SimpleGrid>
-      <Input.Wrapper label="Search ">
-        <Input onChange={(e) => setSearchValue(e.target.value)} />
-      </Input.Wrapper>
+
       <List
         class="list"
         spacing="xs"
@@ -78,7 +79,7 @@ function App() {
           </ThemeIcon>
         }
       >
-        {filteredItems.map(({ name }, index) => (
+        {basketItems.map(({ name }, index) => (
           <List.Item key={index}>{name}</List.Item>
         ))}
       </List>
